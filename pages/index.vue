9233eff7cd571fe03741cb3710621a27084fd79c
krit97s <jakkrit.ss@hotmail.com>
2020-11-19 12:09:17 +0700

    asdasd

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>JAKKRIT SINGSAT - Porfolio</title>
-    </head>
     <section id="main">
       <nav>
         <div class="container-nav">
